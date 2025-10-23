0c3a676298033f028d76558164f7b88943e95e61
Ava Silver <ava.silver@datadoghq.com>
2025-10-23 11:08:52 -0400

    [SVLS-7755] add copyright headers using the script

--- examples/php/src/routes/web.php
+++ examples/php/src/routes/web.php
@@ -1,11 +1,6 @@
 <?php
-
-// Unless explicitly stated otherwise all files in this repository are licensed
-// under the Apache 2.0 License.
-
-// This product includes software developped at
-// Datadog (https://www.datadoghq.com/)
-// Copyright 2025-present Datadog, Inc.
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/) Copyright 2025 Datadog, Inc.
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
